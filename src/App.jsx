01eb0302fe588d73bbea1a53fec785ee1498d41f
Sachith Priyashantha <p.sachith@gtngroup.com>
2024-09-15 00:25:45 +0530

    Add NotFoundPage to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const pageRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
 )
