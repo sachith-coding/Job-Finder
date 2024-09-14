1651b1956cce6a6d550accd7895272ba7b2912b8
Sachith Priyashantha <p.sachith@gtngroup.com>
2024-09-14 23:41:33 +0530

    Setup Page Layout for the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListing from './components/JobListing'
-import ViewAllJobs from './components/ViewAllJobs'
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
 
 const pageRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route index element={<h1>My App</h1>} />
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
   )
 )
 
