4ff480ef3e607537b69853ffb160cc82e1b9e844
Sachith Priyashantha <p.sachith@gtngroup.com>
2024-09-15 00:11:24 +0530

    Replace <a> tags by <Link> tag to stop unwanted page refreshing

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
+import JobsPage from './pages/JobsPage'
 
 const pageRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>
   )
 )
--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const JobItem = ({jobItem}) => {
 
@@ -38,12 +39,12 @@ const JobItem = ({jobItem}) => {
                         <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {jobItem.location}
                     </div>
-                    <a
-                        href={`/job/${jobItem.id}`}
+                    <Link
+                        to={`/job/${jobItem.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         Read More
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
