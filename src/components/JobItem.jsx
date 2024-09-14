6f773ea19cc2fcbdcf1eecf86754e18a5ccced0a
Sachith Priyashantha <p.sachith@gtngroup.com>
2024-09-14 22:57:37 +0530

    Add React Icons Package and use it to Indicate Map Icon

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { FaMapMarker } from 'react-icons/fa'
 
 const JobItem = ({jobItem}) => {
 
@@ -34,7 +35,7 @@ const JobItem = ({jobItem}) => {
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                        <i className="fa-solid fa-location-dot text-lg"></i>
+                        <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {jobItem.location}
                     </div>
                     <a
