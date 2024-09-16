9699549cf585f75ae63ac7cacfde966de59a1edc
Sachith Priyashantha <sachithpriyashantha24@gmail.com>
2024-09-16 23:41:57 +0530

    Conditional Rendering Test

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -10,7 +10,7 @@ const JobListing = ({ isHome = false }) => {
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    {isHome ? 'Recent Jobs' : 'Browse Jobs' }
+                    {isHome ? 'Recent View Jobs' : 'Browse All Jobs' }
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {recentJobs.map((jobItem) => (
